1e0a87dbceb7f1fdae90493f47c6add0850823f8
Batyr Charyev <bcharyev@andiks.com>
2016-09-02 15:41:42 +0300

    add empty cell

--- Assets/game/Scripts/battle/ObjectFactory.cs
+++ Assets/game/Scripts/battle/ObjectFactory.cs
@@ -8,7 +8,7 @@ namespace bc.battle {
         GameObject[] cells = null;
 
         public GameObject GetCell(CellType type) {
-            GameObject result = cells[0];
+            GameObject result = null;
             foreach (GameObject cell in cells) {
                 if (cell.GetComponent<Cell>().type == type) {
                     result = cell;
